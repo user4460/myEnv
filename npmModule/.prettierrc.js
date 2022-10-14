d0781832ed8fe293bc29d0f624d0201ca1165aeb
yoshiaki.n <52188314+user4460@users.noreply.github.com>
2022-10-14 22:39:17 +0800

    config

--- npmModule/.prettierrc.js
+++ npmModule/.prettierrc.js
@@ -3,3 +3,13 @@ module.exports = {
   singleQuote: true,
 };
 //コメント
+{
+  //trailingCommaとは、配列やオブジェクトの最後の要素やプロパティの後ろにカンマをつけるかどうかを指定するオプションです。
+  "trailingComma": "es5",
+  //tabWidthとは、インデントの幅を指定するオプションです。
+    "tabWidth": 4,
+  //semiとは、文末のセミコロンをつけるかどうかを指定するオプションです。
+    "semi": false,
+  //singleQuoteとは、文字列をシングルクォートで囲むかどうかを指定するオプションです。
+        "singleQuote": true
+}
